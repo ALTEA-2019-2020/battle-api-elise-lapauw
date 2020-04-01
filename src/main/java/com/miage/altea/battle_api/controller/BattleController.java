22133276db347b5964272132bd360cf64bba04bd
elise-lapauw <55782687+elise-lapauw@users.noreply.github.com>
2020-04-01 14:24:43 +0200

    ADD cross-origin

--- src/main/java/com/miage/altea/battle_api/controller/BattleController.java
+++ src/main/java/com/miage/altea/battle_api/controller/BattleController.java
@@ -7,8 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
-import java.util.Map;
 import java.util.UUID;
+
 
 @RestController
 @RequestMapping("/battles")
@@ -21,23 +21,26 @@ public class BattleController {
         this.battleService = battleService;
     }
 
+    @CrossOrigin
     @GetMapping()
     public Collection<Battle> getAllBattles() {
         return this.battleService.getAllBattles();
     }
 
-
+    @CrossOrigin
     @PostMapping()
     public Battle battles(@RequestParam(value = "trainer")String trainerName,
                                      @RequestParam(value = "opponent")String opponentName) {
         return this.battleService.createBattle(trainerName, opponentName);
     }
 
+    @CrossOrigin
     @GetMapping(value = "/{uuid}")
     public Battle getBattle(@PathVariable(value = "uuid")UUID uuid){
         return this.battleService.getBattle(uuid);
     }
 
+    @CrossOrigin
     @PostMapping(value = "/{uuid}/{trainerName}/attack")
     public Battle attack(@PathVariable(value = "uuid") UUID uuid, @PathVariable(value = "trainerName") String trainerName){
         return this.battleService.attack(uuid, trainerName);
